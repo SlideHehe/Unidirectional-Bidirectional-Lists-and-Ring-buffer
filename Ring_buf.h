1c9501c0201fbbdb4529afe1381884bb942fb132
Arey <87420088+DarkArey@users.noreply.github.com>
2022-05-21 18:58:48 +0300

    Update Ring_buf.h

--- Ring_buf.h
+++ Ring_buf.h
@@ -9,7 +9,7 @@ using std::endl;
 
 class RingIterator;
 
-class Ring_buf 
+class Ring_buf
 {
 
 private:
@@ -21,8 +21,8 @@ private:
 		Node(int);
 	};
 
-	Node *head;
-	Node *tail;
+	Node* head;
+	Node* tail;
 	int current_size;
 	int max_size;
 
@@ -30,50 +30,52 @@ private:
 
 	bool overflow();
 
-	void quick_sort_array(int* , int*);
+	void quick_sort_array(int*, int*);
 
 public:
 	friend class RingIterator;
 
-	Ring_buf(int );
+	Ring_buf(int);
 
-	Ring_buf(const Ring_buf &);
+	Ring_buf(const Ring_buf&);
 
 	void quick_sort();
 
 	void list_split(Ring_buf&, Ring_buf&, const int&);
 
-	void push(int );
+	void push(int);
 
 	void clear();
 
 	int pop();
 
-	void insert(int , int );
+	void insert(int, int);
+
+	void erase(int);
 
 	RingIterator begin() const;
 
 	RingIterator end() const;
 
 
-	void delete_node(int );
+	void delete_node(int);
 
 	int count();
 
 	void show();
 
-	bool operator == (const Ring_buf& ) const;
+	bool operator == (const Ring_buf&) const;
 
 
-	bool operator != (const Ring_buf& )  const;
+	bool operator != (const Ring_buf&)  const;
 
-	bool operator < (const Ring_buf& ) const;
+	bool operator < (const Ring_buf&) const;
 
-	bool operator <= (const Ring_buf& ) const;
+	bool operator <= (const Ring_buf&) const;
 
-	bool operator > (const Ring_buf& ) const;
+	bool operator > (const Ring_buf&) const;
 
-	bool operator >= (const Ring_buf& ) const;
+	bool operator >= (const Ring_buf&) const;
 
 
 	~Ring_buf();
@@ -82,30 +84,30 @@ public:
 
 class RingIterator {
 public:
-    friend struct Ring_buf::Node;
+	friend struct Ring_buf::Node;
 
-    RingIterator();
+	RingIterator();
 
-    RingIterator(Ring_buf::Node*);
+	RingIterator(Ring_buf::Node*);
 
-    RingIterator(const RingIterator&);
+	RingIterator(const RingIterator&);
 
-    RingIterator &operator++();
+	RingIterator& operator++();
 
-    RingIterator operator++(int);
+	RingIterator operator++(int);
 
-    RingIterator &operator=(const RingIterator&);
+	RingIterator& operator=(const RingIterator&);
 
-    int &operator*() const;
+	int& operator*() const;
 
-    int operator->() const;
+	int operator->() const;
 
-    bool operator==(const RingIterator&) const;
+	bool operator==(const RingIterator&) const;
 
-    bool operator!=(const RingIterator&) const;
+	bool operator!=(const RingIterator&) const;
 
 private:
-    Ring_buf::Node *currNode;
+	Ring_buf::Node* currNode;
 };
 
 

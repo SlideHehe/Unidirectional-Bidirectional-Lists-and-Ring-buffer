55bf8119d094baba2e54ced8ce13e722ee514147
Arey <87420088+DarkArey@users.noreply.github.com>
2022-05-21 18:55:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include "Tests.h"
 
+
 int main() {
+
+    Unidirectional_list_test();
+    //Time_Unidirectional_list_test();
+    cout << "\n--------------------------------\n";
     bdListTest();
+    //Time_bdListTest();
+    cout << "\n--------------------------------\n";
+    Ring_buf_test();
+
+
     return 0;
 }

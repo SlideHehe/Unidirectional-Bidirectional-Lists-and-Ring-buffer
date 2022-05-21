3ab7c906caa8d5fc4f268449cc74f4112bd9be4a
Arey <87420088+DarkArey@users.noreply.github.com>
2022-05-21 19:01:26 +0300

    Update Tests.h

--- Tests.h
+++ Tests.h
@@ -1,8 +1,26 @@
 #ifndef TESTS_H
 #define TESTS_H
 
+#include <chrono>
+using ms = std::chrono::milliseconds;
+
+#include "Unidirectional_list.h"
+#include<forward_list>
+
 #include "BDList.h"
+#include<list>
+
+#include "Ring_buf.h"
+//#include<circular_buffer>
+
+void Unidirectional_list_test();
+void Time_Unidirectional_list_test();
 
 void bdListTest();
+void Time_bdListTest();
+
+
+void Ring_buf_test();
+//void Time_Ring_buf_test();
 
 #endif //TESTS_H
